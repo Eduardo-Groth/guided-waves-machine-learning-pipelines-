51f355610074d66b0a1f6508f49afef2af71a671
Eduardo Becker Groth <groth.mecanica@gmail.com>
2023-03-17 17:51:42 -0300

    nember_defects

--- main.m
+++ main.m
@@ -1,6 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%% GWSG2ML %%%%%%%%%%%%%%%  script by Eduardo Becker Groth
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clc, clear all, close all
 %% requeired infomation
 t_tot=1e-3 ;                    % total time of simulation
 time_interval_monitoring=1e-7;  % acquire step of monitoring signal(s) 
@@ -9,12 +10,12 @@ time_interval_field=0.5e-4;     % acquire step of defauld field data storage (us
 pm_u1=1e-6;                     % initial excitation motion amplitud in x direction
 pm_u2=0;                        % initial excitation motion apmlitud in y direction 
 size_defect=0.5e-3;             % defect size (radius)
-number_defect=25;               % number of defects in the model
+number_defect=[5,30,50];               % number of defects in the model
 
 frequency=100e3;                % tone burst frequency
 number_cycles=5;                % number of cycles 
 
-folder_results = 'C:\Users\Groth\Desktop\models_ml\git_comunication\v0\results' ;  % result files
-folder_files = 'C:\Users\Groth\Desktop\models_ml\git_comunication\v0' ;            % scripts 
+folder_results = 'C:\Users\Groth\Desktop\models_ml\Golden eggs chicken\plane_strip\results' ;  % result files
+folder_files = 'C:\Users\Groth\Desktop\models_ml\Golden eggs chicken\plane_strip' ;            % scripts 
 %%
 operator 
--- operator.m
+++ operator.m
@@ -1,23 +1,33 @@
 %% GWSG2ML - assistant rotine
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for k=1:size(number_defect,2)
+for l=1:2
 fid = fopen('folder.txt','wt');
 fprintf(fid,'%s',folder_results);
 fclose(fid);
 %% save parameters to export for python
-parameters=[t_tot,time_interval_monitoring,time_interval_history,time_interval_field,pm_u1,pm_u2,size_defect,number_defect]; 
+
+cd(folder_files)
+parameters=[t_tot,time_interval_monitoring,time_interval_history,time_interval_field,pm_u1,pm_u2,size_defect,number_defect(k)]; 
 csvwrite('parameters.csv',parameters)
 tone_burst
 %% run ABAQUS
-logical=1;
+logical=0;
 if logical == 1;
     script = ' script';
 else
     script = ' noGUI';
 end
 pathl=strcat('abaqus cae ',script,'="',folder_files,'\','strips.py','"');
-dos(pathl);
 
-%% storage of the signals 
+rr=1;
+while rr>0
+clear rr 
+dos(pathl);
+rr=ans;
+end
+
+    %% storage of the signals 
 cd(folder_results)
 u0= importdata('u0.txt'); 
 figure 
@@ -32,6 +42,6 @@ filenames = fullfile(folder_results, {dinfo.name});
 delete( filenames{:} )
 cd(folder_files)
 delete('folder.txt','abaqus.rpy','abaqus_acis.log','parameters.csv','tb.csv')
-
-
+end
+end
 
